8a1c7432fe8af195d898aaeb2f1ad7b44ac39e68
henryalps@gmail.com <qb473#vPjHkgsGqs>
2016-08-20 22:40:04 +0800

    8.20提交

--- app/src/main/java/com/tencent/streamshare/Utils/Constants.java
+++ app/src/main/java/com/tencent/streamshare/Utils/Constants.java
@@ -4,4 +4,5 @@ package com.tencent.streamshare.Utils;
  * Created by zhaoyongfei on 2016/8/20.
  */
 public class Constants {
+    public final static String URL_USER_LOG_IN = "http://1.tv.ptyg.gitv.tv/i-tvbin/qtv_video/omgtrain/user_login";
 }
