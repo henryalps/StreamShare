d8e89fa7193010222f51f3f64925e5a63d104932
henryrhe <henryalps@gmail.com>
2016-08-21 10:13:12 +0800

    添加网址常量

--- app/src/main/java/com/tencent/streamshare/Utils/Constants.java
+++ app/src/main/java/com/tencent/streamshare/Utils/Constants.java
@@ -4,5 +4,18 @@ package com.tencent.streamshare.Utils;
  * Created by zhaoyongfei on 2016/8/20.
  */
 public class Constants {
-    public final static String URL_USER_LOG_IN = "http://1.tv.ptyg.gitv.tv/i-tvbin/qtv_video/omgtrain/user_login";
+    public final static String URL_USER_LOG_IN =
+            "http://1.tv.ptyg.gitv.tv/i-tvbin/qtv_video/omgtrain/user_login";
+    public final static String URL_LIVE_STREAM_INFO =
+            "http://1.tv.ptyg.gitv.tv/i-tvbin/qtv_video/omgtrain/live_stream_info";
+    public final static String URL_USER_CHECK_STREAM =
+            "http://1.tv.ptyg.gitv.tv/i-tvbin/qtv_video/omgtrain/check_stream";
+    public final static String URL_ATTAIN_BAR_CODE =
+            "http://1.tv.ptyg.gitv.tv/i-tvbin/qtv_video/omgtrain/get_twocode_info";
+    public final static String URL_ATTAIN_STREAM_ADDRESS =
+            "http://1.tv.ptyg.gitv.tv/i-tvbin/qtv_video/omgtrain/get_stream_addr";
+    public final static String URL_GET_PUSH_COMMAND =
+            "http://1.tv.ptyg.gitv.tv/i-tvbin/qtv_video/omgtrain/get_push_command";
+    public final static String URL_EXIT_STREAM =
+            "http://1.tv.ptyg.gitv.tv/i-tvbin/qtv_video/omgtrain/user_exit_stream";
 }
