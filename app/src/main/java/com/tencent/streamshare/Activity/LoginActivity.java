3fd6ef532c4de06398a7267d126b0084ef31814a
brendanlu <brendanlu@tencent.com>
2016-08-21 16:21:27 +0800

    登录页面以及首页的UI修改

--- app/src/main/java/com/tencent/streamshare/Activity/LoginActivity.java
+++ app/src/main/java/com/tencent/streamshare/Activity/LoginActivity.java
@@ -6,7 +6,10 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 import com.tencent.streamshare.MainActivity;
 import com.tencent.streamshare.R;
 
@@ -26,22 +29,30 @@ public class LoginActivity extends Activity {
 		mUserText = (EditText)this.findViewById(R.id.QQ_number);
 		mPassText = (EditText)this.findViewById(R.id.QQ_password);
 		mLoginBtn = (Button) this.findViewById(R.id.btn_qqlogin);
+		mLoginBtn.setFocusable(true);
+		mLoginBtn.setFocusableInTouchMode(true);
+		mLoginBtn.requestFocus();
 		mLoginBtn.setOnClickListener(new View.OnClickListener(){
 
 			public void onClick(View v){
 				mUserid = mUserText.getText().toString();
 				mPasswd = mPassText.getText().toString();
-				doLogin();
+				doLogin(mUserid,mPasswd);
 			}
 
 		});
 	}
-	private void doLogin(){
+	private void doLogin(String userid,String passwd){
 
 		Intent intent = new Intent();
 		intent.setClass(this, MainActivity.class);
 		startActivity(intent);
+//		IntentIntegrator integrator = new IntentIntegrator(LoginActivity.this);
+//		integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE_TYPES);
+//		integrator.setOrientationLocked(false);
+//		integrator.initiateScan();
 
 	}
+
 
 }
---
+++ app/src/main/java/com/tencent/streamshare/Adapter/SteamListAdapter.java
@@ -0,0 +1,16 @@
+package com.tencent.streamshare.Adapter;
+
+import android.accounts.Account;
+import android.content.Context;
+
+/**
+ * Created by Administrator on 2016/8/21.
+ */
+public class SteamListAdapter {
+
+
+	public SteamListAdapter(Context context, Account Account){
+
+	}
+
+}
